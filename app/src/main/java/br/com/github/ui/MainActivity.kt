bb5ca9973f4a38f8949d3e7fc711551caa414c33
peterson.farias <petersongfarias@gmail.com>
2023-05-20 22:26:25 -0300

    finalizando camada de network de network

---
+++ app/src/main/java/br/com/github/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package br.com.github.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
