77e92d14108e7bd942d861d27ec297cd35380c4c
peterson.farias <petersongfarias@gmail.com>
2023-05-21 23:25:13 -0300

    Implementando a home

--- app/src/main/java/br/com/github/domain/base/ResultState.kt
+++
@@ -1,6 +0,0 @@
-package br.com.github.domain.base
-
-sealed class ResultState<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : ResultState<T>(data)
-    class Error<T>(message: String?) : ResultState<T>(message = message)
-}
