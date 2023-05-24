f86eb500ca3ee71bd8cafb8f9fcac26740f2b6e8
peterson.farias <petersongfarias@gmail.com>
2023-05-23 22:44:17 -0300

    ajustando loading

--- app/src/main/java/br/com/github/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/github/ui/home/HomeFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
